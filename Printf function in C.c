1af6ff88a087daa4ab3651e07fc9bd0d62c3c261
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-18 10:03:23 +0500

    programmed successfully

---
+++ Printf function in C.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    // printf function is used to print output on screen it can be a message or anything
+    // syntax
+    printf("");
+    // in quotes message will be written printed on screen
+}

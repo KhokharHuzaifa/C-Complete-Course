4299dbc2403770f805d6214702370012c728a85c
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-19 12:47:36 +0500

    Program Added Successfully

---
+++ Data types in C.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    // variables are used to store value in memory and call them by their name
+    // variable name can be any name based on some rules
+    int var1=20;
+    int var2=10;
+    printf("Sum of two varibales is %d",var1+var2);
+}

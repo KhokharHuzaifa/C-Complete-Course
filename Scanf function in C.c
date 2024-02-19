93b43d6a8d866ad71e5ee5598a7f7e3ca45eec4d
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-19 12:40:26 +0500

    programmed added successfully

---
+++ Scanf function in C.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int a;
+    printf("Enter any number :");
+    scanf("%d",&a);
+    printf("You typed %d",a);
+    // scanf is used to read values from user during run time
+}

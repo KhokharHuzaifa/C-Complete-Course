1269b6ea830dae1b6472ea46143d71f8b00fc18a
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-20 06:00:09 +0500

    added Successfully

---
+++ Datatypes in C.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+// Create All variables
+int myNum = 5;             // Integer (whole number)
+float myFloatNum = 5.99;   // Floating point number
+char myLetter = 'D';       // Character
+
+// Print variables
+printf("%d", myNum);
+printf("%f", myFloatNum);
+printf("%c", myLetter);
+}
+// character datatype variable
+char myGrade = 'A';
+printf("%c", myGrade);
+// number datatype variable
+int myNum = 1000;
+printf("%d", myNum);

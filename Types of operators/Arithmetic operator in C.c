890ad7e8b688fe36ea405958f0e76a344a3c7740
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-21 12:15:37 +0500

    Arithmentic operators

---
+++ Types of operators/Arithmetic operator in C.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    // Arithmetic operators are used to perform common mathematical operations.
+    int x = 12,y = 3;
+    printf("%d", x + y);    // addition
+    printf("%d", x - y);    //subtraction
+    printf("%d", x * y);    // multiplication
+    printf("%d", x / y);    // division
+    printf("%d", x % y);    // modulus
+    printf("%d", ++x);      // increment
+    printf("%d", --x);      // decrement
+
+
+}

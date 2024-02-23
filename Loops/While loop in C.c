6ce335735f0f688c4a29aa576111bbb4411f4461
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-23 09:30:54 +0500

    Loops in C

---
+++ Loops/While loop in C.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    // The while loop loops through a block of code as long as a specified condition is true
+    // Syntax
+    while (condition) {
+    // code block to be executed
+    }
+
+    // Example
+    int i = 0;
+    while (i < 5) {
+    printf("%d\n", i);
+    i++;
+    // In the example the code in the loop will run, over and over again, as long as a variable (i) is less than 5
+}
+}

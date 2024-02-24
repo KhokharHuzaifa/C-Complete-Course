09b1bd785260dbdc44a59f6cf9c5ed2e3062ccfe
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-24 10:37:14 +0500

    Added

---
+++ Array/Using loop in array in C.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    // You can loop through the array elements with the for loop.
+    // The following example outputs all elements in the myNumbers array:
+    // Example
+    int myNumbers[] = {25, 50, 75, 100};
+    for (int i = 0; i < 4; i++) {
+    printf("%d\n", myNumbers[i]);
+    }
+}

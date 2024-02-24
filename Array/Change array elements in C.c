7d173caf61f34dd2910e193f15599bbb2c874bf0
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-24 10:31:52 +0500

    Added

---
+++ Array/Change array elements in C.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+
+    // Change an Array Element
+    // To change the value of a specific element, refer to the index number:
+    // Example
+    int myNewNumbers[] = {25, 50, 75, 100};
+    myNewNumbers[0] = 33;
+    printf("%d", myNumbers[0]);
+    // Now outputs 33 instead of 25
+}

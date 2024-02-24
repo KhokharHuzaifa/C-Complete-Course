a5d83459fa6618f1b066c91bad48c9d92e456680
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-24 10:21:46 +0500

    Arrays

---
+++ Array/Array in C.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    // Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+    // To create an array, define the data type (like int) and specify the name of the array followed by square brackets [].
+    // To insert values to it, use a comma-separated list, inside curly braces:
+    int myNumbers[] = {25, 50, 75, 100};
+}

cd63a9045d8aa04caa8de986b9f6582e4a07f6d0
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-24 10:41:35 +0500

    Arrays Complete

---
+++ Array/Set array size in C.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    // Set Array Size
+    // Another common way to create arrays, is to specify the size of the array, and add elements later:
+    // Example
+    // Declare an array of four integers:
+    int myNumbers[4];
+    // Add elements
+    myNumbers[0] = 25;
+    myNumbers[1] = 50;
+    myNumbers[2] = 75;
+    myNumbers[3] = 100;
+    printf("%d",myNumbers)
+}

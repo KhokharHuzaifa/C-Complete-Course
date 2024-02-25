616eb75c120a47569e08033abe1f74c98840f702
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-25 10:27:46 +0500

    String Array added

---
+++ String Array/Strings array in C.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    // Strings are used for storing text/characters.
+    // For example, "Hello World" is a string of characters
+    char greetings[] = "Hello World!";
+}

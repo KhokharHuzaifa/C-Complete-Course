82732bc606d5750ca39714f1902ddcb49aaa2a0c
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-25 10:43:06 +0500

    Loop in string array

---
+++ String Array/Loop in string array in C.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    // You can also loop through the characters of a string, using a for loop:
+    // Example
+    char carName[] = "Volvo";
+    for (int i = 0; i < 5; ++i) {
+    printf("%c\n", carName[i]);
+}
+}

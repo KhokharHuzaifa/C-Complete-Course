917951faa84363fc06ec607fb03935405b0d8896
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-26 08:55:59 +0500

    StrCat Added

---
+++ String functions/Strcat function in C.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <string.h>
+// you must include the <string.h> header file in your program
+int main(){
+// To concatenate (combine) two strings, you can use the strcat() functionchar alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char str1[20] = "Hello ";
+char str2[] = "World!";
+// Concatenate str2 to str1 (result is stored in str1)
+strcat(str1, str2);
+// Print str1
+printf("%s", str1);
+// Note that the size of str1 should be large enough to store the result of the two strings combined 
+}

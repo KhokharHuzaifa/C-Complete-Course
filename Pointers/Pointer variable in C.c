211955ba5773483d5fa2c3e850bb8d61dc02dfb9
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-27 23:56:03 +0500

    code added

---
+++ Pointers/Pointer variable in C.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+// A pointer is a variable that stores the memory address of another variable as its value.
+// A pointer variable points to a data type (like int) of the same type, and is created with the * operator.
+// The address of the variable you are working with is assigned to the pointer
+// Example
+int myAge = 43;     // An int variable
+int* ptr = &myAge;  // A pointer variable, with the name ptr, that stores the address of myAge
+
+// Output the value of myAge (43)
+printf("%d\n", myAge);
+
+// Output the memory address of myAge (0x7ffe5367e044)
+printf("%p\n", &myAge);
+
+// Output the memory address of myAge with the pointer (0x7ffe5367e044)
+printf("%p\n", ptr);
+}

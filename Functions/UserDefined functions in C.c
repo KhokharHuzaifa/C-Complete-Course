68a627a56e147b5bbd416055f113986aa7587245
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-28 08:46:10 +0500

    Functions

---
+++ Functions/UserDefined functions in C.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    // To create (often referred to as declare) your own function, specify the name of the function, followed by 
+    // parentheses () and curly brackets {}
+    // Syntax
+void myFunction() {
+  // code to be executed
+}
+}

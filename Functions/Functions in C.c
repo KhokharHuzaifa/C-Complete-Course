cb9957eb27e069ddf78eb4c46132172a5635eb71
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-28 08:40:14 +0500

    Functions

---
+++ Functions/Functions in C.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    // A function is a block of code which only runs when it is called.
+// You can pass data, known as parameters, into a function.
+// Functions are used to perform certain actions, and they are important for reusing code: Define the code once, and use it many times
+// Here main act as a function
+}

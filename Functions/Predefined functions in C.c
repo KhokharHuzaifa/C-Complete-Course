8a64f01e8f1efa3effb52313e8659721d052da9a
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-02-28 08:43:18 +0500

    Functions

---
+++ Functions/Predefined functions in C.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    // main() is a function, which is used to execute code, and 
+    // printf() is a function; used to output/print text to the screen
+    // Example
+int main() {
+  printf("Hello World!");
+  return 0;
+}
+}

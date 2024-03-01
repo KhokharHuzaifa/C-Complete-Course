81c0d82b604ed36f7d20ebc7d9acb6d70bb57c4c
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-01 11:31:34 +0500

    Math functions

---
+++ Math Functions/Round a number function in C.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+//    The ceil() function rounds a number upwards to its nearest integer, and the floor() method rounds a 
+//    number downwards to its nearest integer, and returns the result
+// Example
+printf("%f", ceil(1.4)); // output 2
+printf("%f", floor(1.4)); // output 1
+}
---
+++ Math Functions/Square Root function in C.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    // To find the square root of a number, use the sqrt() function
+    printf("%f", sqrt(16));
+}

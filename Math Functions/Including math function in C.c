6dea8e3561338796b02ec349d8c06ae55e7f36c8
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-01 10:55:37 +0500

    Math fuctions

---
+++ Math Functions/Including math function in C.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include <math.h>
+int main(){
+    // There is also a list of math functions available, that allows you to perform mathematical tasks on numbers.
+    // To use them, you must include the math.h header file in your program
+    // #include <math.h>
+}

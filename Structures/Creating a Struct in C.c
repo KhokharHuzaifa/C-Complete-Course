27bc6f1337f50f8929d9df9b6c15a8b9cca7b15a
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-04 12:31:56 +0500

    Structures

---
+++ Structures/Creating a Struct in C.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    // Structures (also called structs) are a way to group several related variables into one place. Each variable 
+    // in the structure is known as a member of the structure
+
+    // Create a Structure
+// You can create a structure by using the struct keyword and declare each of its members inside curly braces:
+
+struct MyStructure {   // Structure declaration
+  int myNum;           // Member (int variable)
+  char myLetter;       // Member (char variable)
+}; // End the structure with a semicolon
+}

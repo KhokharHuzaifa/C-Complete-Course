c84eec39da37e2a313f5ab46d335479691592d63
Huzaifa Khokhar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-05-08 22:33:42 +0500

    Update Modify string array in C.c

--- String Array/Modify string array in C.c
+++ String Array/Modify string array in C.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     // To change the value of a specific character in a string, refer to the index number, and use single quotes
-    // Ex:
+    // Exa:
     char greetings[] = "Hello World!";
     greetings[0] = 'J';
     printf("%s", greetings);

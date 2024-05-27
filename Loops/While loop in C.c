27d95cf5fb16b7bca12085bd58ccdf797518be9a
Huzaifa Khokhar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-05-27 23:40:18 +0500

    Update While loop in C.c

--- Loops/While loop in C.c
+++ Loops/While loop in C.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     // The while loop loops through a block of code as long as a specified condition is true
-    // Syntax
+    // Sy
     while (condition) {
     // code block to be executed
     }

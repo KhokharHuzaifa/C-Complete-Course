5f9012542bba32066250e9d8f8bc5a00e771f905
Huzaifa Khokhar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-06-06 22:40:13 +0500

    Update Access array elements in C.c

--- Array/Access array elements in C.c
+++ Array/Access array elements in C.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
 
-    // Access the Elements of an Array
+    // Ac the Elements of an Array
     // To access an array element, refer to its index number.
     // Array indexes start with 0: [0] is the first element. [1] is the second element, etc.
     printf("%d", myNumbers[0]);

04f538292696f3fb61af21c56f33b493218887d6
Huzaifa Khokhar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-06-09 22:38:38 +0500

    Update Functions in C.c

--- Functions/Functions in C.c
+++ Functions/Functions in C.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    // A funct is a block of code which only runs when it is called.
+    // A functio is a block of code which only runs when it is called.
 // You can pass data, known as parameters, into a function.
 // Functions are used to perform certain actions, and they are important for reusing code: Define the code once, and use it many times
 // Here main act as a function

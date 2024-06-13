88e9cdbfc5458f62732044759684b07055884c8d
Huzaifa Khokhar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-06-13 23:42:37 +0500

    Update Square Root function in C.c

--- Math Functions/Square Root function in C.c
+++ Math Functions/Square Root function in C.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    // To find the squ root of a number, use the sqrt() function
+    // To find the squa root of a number, use the sqrt() function
     printf("%f", sqrt(16));
 }

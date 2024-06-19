26382ba807b4920f1f028a76afd3497798f72345
Huzaifa Khokhar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-06-19 23:11:07 +0500

    Update Pointer variable in C.c

--- Pointers/Pointer variable in C.c
+++ Pointers/Pointer variable in C.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-// A poi is a variable that stores the memory address of another variable as its value.
+// A poin is a variable that stores the memory address of another variable as its value.
 // A pointer variable points to a data type (like int) of the same type, and is created with the * operator.
 // The address of the variable you are working with is assigned to the pointer
 // Example

0c63f4b903fd7ee565c5fe83412e13b3ac105093
Huzaifa Khokhar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-06-21 21:58:08 +0500

    Update Else statements in C.c

--- Conditional statements C/Else statements in C.c
+++ Conditional statements C/Else statements in C.c
@@ -3,7 +3,7 @@ int main(){
     // Use the else statement to specify a block of code to be executed if the condition is false.
     // Syntax :
     if (condition) {
-    // bl of code to be executed if the condition is true
+    // blo of code to be executed if the condition is true
     } else {
     // block of code to be executed if the condition is false
     }

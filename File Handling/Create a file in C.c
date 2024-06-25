4632c3c87e9233ff61cb3e5bd9d2c33aabe84c54
Huzaifa Khokhar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-06-25 23:50:03 +0500

    Update Create a file in C.c

--- File Handling/Create a file in C.c
+++ File Handling/Create a file in C.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    // To create a file, you can use the w mode inside the fopen() function
+    // To create a e, you can use the w mode inside the fopen() function
     // Example
 FILE *fptr;
 // Create a file

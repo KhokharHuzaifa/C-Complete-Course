26befcae9e6e280fe67967e5949647981bd3b40c
Huzaifa Khokhar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-06-27 23:54:28 +0500

    Update Do while loop in C.c

--- Loops/Do while loop in C.c
+++ Loops/Do while loop in C.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    // The do/while loop is a variant of the while loop. This loop will execute the code block once, before 
+    // The do/while loop is a va of the while loop. This loop will execute the code block once, before 
     // checking if the condition is true, then it will repeat the loop as long as the condition is true.
     
     // Syntax

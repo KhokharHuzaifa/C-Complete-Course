8b52cc694b325e4790e5b22769c660fd463197a1
Huzaifa Zafar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-07-12 22:06:53 +0500

    Update Break and Continue statement in C.c

--- Break and Continue statement in C.c
+++ Break and Continue statement in C.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     // Break
-    // You h already seen the break statement used in an earlier chapter of this tutorial. It was used to "jump out" 
+    // You ha already seen the break statement used in an earlier chapter of this tutorial. It was used to "jump out" 
     // of a switch statement.
     // The break statement can also be used to jump out of a loop.
 

aab0154b4536ba826eb88f9d37bab9303a594843
Huzaifa Zafar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-07-14 23:40:29 +0500

    Update Strcmp function in C.c

--- String functions/Strcmp function in C.c
+++ String functions/Strcmp function in C.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include <string.h>
-// you must include the <string.h> header file in your pro
+// you must include the <string.h> header file in your prog
 int main(){
 // To compare two strings, you can use the strcmp() functionchar alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 // It returns 0 if the two strings are equal

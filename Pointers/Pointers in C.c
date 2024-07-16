463efd26399b119c752e4faeae6b992a74aab6bd
Huzaifa Zafar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-07-16 23:51:09 +0500

    Update Pointers in C.c

--- Pointers/Pointers in C.c
+++ Pointers/Pointers in C.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    // Creating Poi
+    // Creating Poin
 // You learned from the previous topic, that we can get the memory address of a variable with the reference operator &
 
 // Example

cf81a70ea49463d4e159bd4abe159e83537dc71b
Huzaifa Zafar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-07-24 23:30:10 +0500

    Constants in C.c

--- Constants in C.c
+++ Constants in C.c
@@ -2,7 +2,7 @@
 #include<conio.h>
 int main(){
     //If you don't want others (or yourself) to change existing variable values, you can use the const keyword.
-    //This will decl the variable as "constant", which means unchangeable and read-only
+    //This will declares the variable as "constant", which means unchangeable and read-only
     const int myNum = 15;  // myNum will always be 15
     myNum = 10;  // error: assignment of read-only variable 'myNum'
     const int minutesPerHour = 60;

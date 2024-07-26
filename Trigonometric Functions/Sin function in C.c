21157c742d11dd7a80b5d98301fe8ae3c6399bca
Huzaifa Zafar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-07-26 23:37:00 +0500

    Sin function in C.c

--- Trigonometric Functions/Sin function in C.c
+++ Trigonometric Functions/Sin function in C.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 int main() {
-    // sin(x)	Retu the sine of x (x is in radians)
+    // sin(x)	Returns the sine of x (x is in radians)
   printf("%f", sin(90));
   return 0;
 }

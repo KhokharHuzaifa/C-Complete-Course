7494e2a3141396673c6796d7aa5eeb74895cfaa0
Huzaifa Zafar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-08-16 23:58:35 +0500

    Strcpy function in C.c

--- String functions/Strcpy function in C.c
+++ String functions/Strcpy function in C.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include <string.h>
-// you must inclu the <string.h> header file in your program
+// you must include the <string.h> header file in your program
 int main(){
 // To copy the value of one string to another, you can use the strcpy() functionchar str1[20] = "Hello ";
 char str1[20] = "Hello World!";

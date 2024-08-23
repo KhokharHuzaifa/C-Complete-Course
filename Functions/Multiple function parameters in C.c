76bb0c7f9fcd2bd5eacf5ebe63ee6bf9b08d6017
Huzaifa Zafar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-08-23 23:34:14 +0500

    Multiple function parameters in C.c

--- Functions/Multiple function parameters in C.c
+++ Functions/Multiple function parameters in C.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    // Inside the function, you can add as many parameters as you wan
+    // Inside the function, you can add as many parameters as you want
     // Example
 void myFunction(char name[], int age) {
   printf("Hello %s. You are %d years old.\n", name, age);
